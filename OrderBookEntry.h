ce963833e57804bde90e8734da90c50c63e44afc
Arsyucha 99 <dmitrievarsenii1@gmail.com>
2024-03-13 14:43:05 +0100

    main files

---
+++ OrderBookEntry.h
@@ -0,0 +1,12 @@
+#include <string>
+
+enum class OrderBookType{bid, ask};
+
+class OrderBookEntry {
+    public: 
+        OrderBookEntry(double _price, double _amount, std::string _timestamp, std::string _product, OrderBookType _orderType);
+
+        double price, amount;
+        std::string timestamp, product;
+        OrderBookType orderType;
+};
