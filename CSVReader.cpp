45ff566747df69c4c15f55a602106b4cae531e2a
Arsyucha99 <240329@vutbr.cz>
2024-07-08 21:06:24 +0200

    CSVReader functions changes

--- CSVReader.cpp
+++ CSVReader.cpp
@@ -1,4 +1,5 @@
 #include "CSVReader.h"
+#include <iostream>
 
 CSVReader::CSVReader()
 {
@@ -15,6 +16,20 @@ std::vector<std::string> CSVReader::tokenise (const std::string csvLine, const c
     return tokens;
 }
 
-OrderBookEntry CSVReader::str_to_OBE (std::vector<std::string> strings) {
-    OrderBookEntry obe{1, 1, "", "", OrderBookType::bid};
+OrderBookEntry CSVReader::str_to_OBE (std::vector<std::string> tokens) {
+    double price, amount;
+    if (tokens.size() != 5) {
+        std::cout << "Bad line " << std::endl;
+        throw std::exception{};
+    }
+    try {
+        double price = stod(tokens[3]), amount = stod(tokens[4]);
+        std::cout << "Price: " << price << ", amount: " << amount << std::endl;
+    } catch(std::exception& e) {
+        std::cout << "Bad float! " << tokens[3];
+        std::cout << "Bad float! " << tokens[4];
+        throw;
+    }
+    OrderBookEntry obe{price, amount, tokens[0], tokens[1], OrderBookEntry::str_to_OrderBooktype(tokens[2])};
+    return obe;
 }
